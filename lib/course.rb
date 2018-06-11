7af9a6e17249bb701fd70e9c53b39ff215efb1e4
Mary-Kate James <marykatj@gmail.com>
2018-06-11 01:56:49 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
+
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule
+    @description = description
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
 
