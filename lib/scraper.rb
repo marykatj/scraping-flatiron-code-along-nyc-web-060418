bc7b500565da10370cf0d16f4fafdd4077a28816
Mary-Kate James <marykatj@gmail.com>
2018-06-11 02:01:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,8 +20,6 @@ class Scraper
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
-  end
-
   def get_courses
       self.get_page.css(".post")
     end
